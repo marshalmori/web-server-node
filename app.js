1f721a7cc11e2aa37e261d163398c6fb5a82e5e7
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-10-06 11:01:56 -0300

    Mudanças para usar React

--- app.js
+++ app.js
@@ -12,29 +12,29 @@ app.set("view engine", "hbs");
 // Servir conteudo estático, aqui serve o / com o index.html
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home", {
-    nombre: "Marshal Mori Cavalheiro",
-    titulo: "App com Node",
-  });
-});
+// app.get("/", (req, res) => {
+//   res.render("home", {
+//     nombre: "Marshal Mori Cavalheiro",
+//     titulo: "App com Node",
+//   });
+// });
 
-app.get("/generic", (req, res) => {
-  res.render("generic", {
-    nombre: "Marshal Mori Cavalheiro",
-    titulo: "App com Node",
-  });
-});
+// app.get("/generic", (req, res) => {
+//   res.render("generic", {
+//     nombre: "Marshal Mori Cavalheiro",
+//     titulo: "App com Node",
+//   });
+// });
 
-app.get("/elements", (req, res) => {
-  res.render("elements", {
-    nombre: "Marshal Mori Cavalheiro",
-    titulo: "App com Node",
-  });
-});
+// app.get("/elements", (req, res) => {
+//   res.render("elements", {
+//     nombre: "Marshal Mori Cavalheiro",
+//     titulo: "App com Node",
+//   });
+// });
 
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/public/404.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
